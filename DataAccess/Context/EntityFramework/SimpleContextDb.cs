c25aef941e5cc045f3f81bb8c474bbd233d25bf1
unknown <oyurt43@gmail.com>
2023-04-22 01:13:07 +0300

    ilk

---
+++ DataAccess/Context/EntityFramework/SimpleContextDb.cs
@@ -0,0 +1,19 @@
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Context.EntityFramework
+{
+    public class SimpleContextDb : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-JA53S8S;Database=b2b;Integrated Security=true;");
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
+        public DbSet<EmailParameter> EmailParameters { get; set; }
+        public DbSet<Basket> Baskets { get; set; }
+    }
+}
